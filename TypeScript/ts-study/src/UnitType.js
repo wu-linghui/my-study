20eb6e345e19eda63613bfa6251846a403169fe0
wu-linghui <1540996580@qq.com>
2021-05-14 00:06:11 +0800

    typescript

---
+++ TypeScript/ts-study/src/UnitType.js
@@ -0,0 +1,7 @@
+"use strict";
+/* 当 TypeScript 不确定一个联合类型的变量到底是哪个类型的时候，
+   我们只能访问此联合类型的所有类型里共有的属性或方法 */
+function getLenght(something) {
+    // return something.length; // length 不是 string 和 number 的共有属性，所以会报错
+    return something.toString(); // string 和 number 的共有属性是没问题的
+}
